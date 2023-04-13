0e2dede40318255744b2fd8fee59c728ba7d6a77
Said Ravest <said.tattoo@gmail.com>
2023-04-13 01:24:15 -0400

    ruta mascotas

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.json({resp:'ok'})
 })
-
+app.get('/animales',(req, res)  => {
+    res.json({resp:'mascotas'})
+})
 app.listen(process.env.PORT || 3000)
